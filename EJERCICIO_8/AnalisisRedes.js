17df2d6254599451f82a6d71f06bd1be495988d2
alfercar <alfonsofdez97@gmail.com>
2022-02-24 16:36:42 +0100

    8

--- EJERCICIO_8/AnalisisRedes.js
+++ EJERCICIO_8/AnalisisRedes.js
@@ -41,12 +41,52 @@ require([
       // Parse DOM nodes decorated with the data-dojo-type attribute
       parser.parse();
 
+      /*
+             * Step: create an array of JSON objects that will be used to create print templates
+             */
+
+      /*     var myLayouts = [{
+       "name" : "Letter ANSI A Landscape",
+       "label" : "Landscape (PDF)",
+       "format" : "pdf",
+       "options" : {
+       "legendLayers" : [], // empty array means no legend
+       "scalebarUnit" : "Miles",
+       "titleText" : "Landscape PDF"
+       }
+       }, {
+       "name" : "Letter ANSI A Portrait",
+       "label" : "Portrait (JPG)",
+       "format" : "jpg",
+       "options" : {
+       "legendLayers" : [],
+       "scaleBarUnit" : "Miles",
+       "titleText" : "Portrait JPG"
+       }
+       }];
+
+       */
+      /*
+       * Step: create the print templates
+       */
+
+      /*    var myTemplates = [];
+       dojo.forEach(myLayouts, function(lo) {
+       var t = new PrintTemplate();
+       t.layout = lo.name;
+       t.label = lo.label;
+       t.format = lo.format;
+       t.layoutOptions = lo.options
+       myTemplates.push(t);
+       });
+       */
+
       // Create the map
 
       mapMain = new Map("cpCenter", {
-          basemap: "topo",
-          center: [-117.19, 34.05],
-          zoom: 12
+        basemap: "topo",
+        center: [-117.19, 34.05],
+        zoom: 12
       });
 
       /*
@@ -54,17 +94,17 @@ require([
        */
 
       var directions = new Directions({
-          map : mapMain,
-          routeTaskUrl : "http://utility.arcgis.com/usrsvcs/appservices/OM1GNiiACNJceMRn/rest/services/World/Route/NAServer/Route_World"
-          }, "divDirections");
+        map: mapMain,
+        routeTaskUrl: "http://utility.arcgis.com/usrsvcs/appservices/OM1GNiiACNJceMRn/rest/services/World/Route/NAServer/Route_World"
+      }, "divDirections");
 
-          directions.startup();
+      directions.startup();
 
       /*
        * Step: Add the Print widget
        */
 
 
-  });
+    });
 
-});
+  });
